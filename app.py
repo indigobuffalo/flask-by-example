9f3aa25bbc4ef619011c8f9a4b81d157799135b7
indigobuffalo <stephen.akerson@gmail.com>
2020-04-19 13:10:05 -0700

    add postges, postgres migrations

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import Result
 
 
 @app.route('/')
@@ -14,9 +19,6 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
-
-
-print(f'App settings are: {os.environ["APP_SETTINGS"]}')
 
 
 if __name__ == '__main__':
